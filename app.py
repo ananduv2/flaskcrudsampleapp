cfdfa7d28bc8c8e295a4dca1b3cb139d326e07ac
ananduv2 <ananduv2.0@gmail.com>
2021-06-17 15:14:25 +0530

    CRUD operations working

--- app.py
+++ app.py
@@ -26,21 +26,67 @@ def enter():
        finally:
            return render_template('msg.html',msg=msg)
 
+
+
 @app.route('/')
+def contact():
+   conn =sqlite3.connect('test.db')
+   conn.row_factory = sqlite3.Row
+   cur=conn.cursor()
+   cur.execute("SELECT * FROM STUDENT")
+   rows = cur.fetchall()
+   return render_template("list.html",rows = rows)
+
+
+
+@app.route('/edit/<id>')
+def edit(id):
+    conn =sqlite3.connect('test.db')
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM STUDENT WHERE ID=(?)",(id))
+    rows = cur.fetchone()
+    return render_template("edit.html",id=rows[0],name=rows[1],city=rows[2])
+
+
+@app.route('/update/<id>',methods=['POST','GET'])
+def update(id):
+    sid=id
+    name=request.form['name']
+    city=request.form['city']
+    try:
+        conn =sqlite3.connect('test.db')
+        cur=conn.cursor()
+        cur.execute("UPDATE STUDENT SET NAME=(?),CITY=(?) WHERE ID=(?) ",(name,city,sid))
+        conn.commit()
+        msg="Updated successfully"
+    except:
+        conn.rollback()
+        msg ="Updation failed"
+    finally:
+        return render_template('msg.html',msg=msg)
+
+
+@app.route('/delete/<id>')
+def delete(id):
+   sid=id
+   try:
+        conn =sqlite3.connect('test.db')
+        cur=conn.cursor()
+        cur.execute("DELETE FROM STUDENT WHERE ID=(?) ",(sid))
+        conn.commit()
+        msg="Deleted successfully"
+   except:
+        conn.rollback()
+        msg ="Deletion failed"
+   finally:
+        return render_template('msg.html',msg=msg)
+
+   
+
+
+@app.route('/create')
 def create():
    return render_template('enter.html')
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
